a0a4c6b5d994fe439b55d8e0603b29472efbb972
He_does_not_DICE <95764398+HedoesnotDICE@users.noreply.github.com>
2026-04-01 22:13:38 +0800

    删除mongoDB

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,11 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import Router from '@koa/router'
 import Redis from 'ioredis';
-import mongoose from 'mongoose';
 import assert from "assert";
 
-// 初始化各服务的连接 redis, mongo
+// 初始化各服务的连接 redis
 async function initService() {
-    const {REDIS_ADDRESS, REDIS_USERNAME, REDIS_PASSWORD, MONGO_ADDRESS, MONGO_USERNAME, MONGO_PASSWORD} = process.env;
+    const {REDIS_ADDRESS, REDIS_USERNAME, REDIS_PASSWORD} = process.env;
     const [ REDIS_HOST, REDIS_PORT] = REDIS_ADDRESS.split(':');
     const redis = new Redis({
         port: parseInt(REDIS_PORT, 10),
@@ -19,25 +18,15 @@ async function initService() {
 
     assert(await redis.echo('echo') === 'echo', `redis echo error`);
 
-    const mongoUrl = `mongodb://${MONGO_USERNAME}:${encodeURIComponent(MONGO_PASSWORD)}@${MONGO_ADDRESS}`;
-    await mongoose.connect(mongoUrl);    
-
     return {
         redis,
-        mongoose,
     }
 }
 
-initService().then(async ({ redis, mongoose}) => {
-    const kittySchema = new mongoose.Schema({
-        name: String
-    });
-
-    const Kitten = mongoose.model('Kitten', kittySchema);
-
+initService().then(async ({ redis }) => {
     const app = new Koa();
-
     const router = new Router();
+    
     router.get('/', ctx => {
         ctx.body = `Nodejs koa demo project`;
     }).get('/api/get_data_from_redis', async(ctx) => {
@@ -66,33 +55,6 @@ initService().then(async ({ redis, mongoose}) => {
         ctx.body = {
             success: true,
         }
-    }).get('/api/get_data_from_mongodb', async(ctx) => {
-        const name = ctx.query.name as string;
-        assert(name?.trim(), `name is required`);
-        const data = await Kitten.findOne({ name});
-        
-        if (data) {
-            ctx.body = {
-                success: true,
-                data: data.toJSON(),
-            }
-        } else {
-            ctx.status = 404;
-            ctx.body = {
-                success: false,
-                message: `${name} not exist`,
-            }
-        }
-    }).post('/api/set_data_to_mongodb', async(ctx) => {
-        const name = ctx.query.name as string;
-        assert(name?.trim(), `name is required`);
-
-        const kit = new Kitten({ name });
-        await kit.save();
-
-        ctx.body = {
-            success: true,
-        }
     });
 
     app.use(bodyParser());
@@ -103,4 +65,4 @@ initService().then(async ({ redis, mongoose}) => {
         console.log(`Server running on port ${PORT}`);
     });
 
-}).catch((error: string) => console.log("Init service  error: ", error));
+}).catch((error: string) => console.log("Init service error: ", error));
